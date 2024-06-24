d69a2153cb6fe6294fa7682d1f92efad4be58224
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-24 18:40:51 +0545

    ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import Header from "./containers/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductComponent from "./containers/ProductComponent";
+import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>Hello</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductListing />}></Route>
+          <Route path="/product/:productId" element={<ProductDetail />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
