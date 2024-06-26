5e83a31d9bb5d24cb3ac2e9dc1832bd47070f247
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-26 19:31:29 +0545

    using action to fetch api

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
-import axios from "axios";
-import { setProducts } from "../redux/actions/productActions";
+
+import { fetchProducts } from "../redux/actions/productActions";
 
 function ProductListing() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.allProducts.products);
 
-  const fetchProducts = async () => {
-    const response = await axios
-      .get("https://fakestoreapi.com/products")
-      .catch((err) => console.log("Error", err));
-    dispatch(setProducts(response.data));
-  };
+  // const fetchProducts = async () => {
+  //   const response = await axios
+  //     .get("https://fakestoreapi.com/products")
+  //     .catch((err) => console.log("Error", err));
+  //   dispatch(setProducts(response.data));
+  // };
 
   useEffect(() => {
-    fetchProducts();
+    dispatch(fetchProducts());
   }, [dispatch]);
   console.log("Products", products);
 
--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,10 +1,22 @@
 import { ActionTypes } from "../constants/product_types";
+import axios from "axios";
+
+export const fetchProducts = async () => {
+  const response = await axios.get("/products");
+  console.log(response);
+
+  return {
+    type: ActionTypes.FETCH_PRODUCTS,
+    payload: response,
+  };
+};
 
 export const setProducts = (products) => {
   return {
     type: ActionTypes.SET_PRODUCTS,
     payload: products,
   };
+  k;
 };
 
 export const selectedProduct = (product) => {
